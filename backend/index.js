32b920e75597f54fe2b3e6dc8807ceb8c9f8d9f0
vikash <vp946203@gmail.com>
2025-11-13 15:51:43 +0530

    solve some issue

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,16 @@ app.use(cors({
 app.use(express.json({limit:"40kb"}))
 app.use(express.urlencoded({limit:"40kb", extended:true}))
 
-app.use("/api/v1/user",userRouter)
+app.use("/api/v1/users",userRouter)
 
 const port = process.env.PORT;
 const url = process.env.MONGO_URL;
 
-
+const path = require("path");
+app.use(express.static(path.join(__dirname, "frontend"))); 
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 
 async function start() {
     await mongoose.connect(url);
